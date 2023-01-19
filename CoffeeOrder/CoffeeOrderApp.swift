432f058e8bf70f1da30c667a7d596ddf4698b943
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-19 15:36:04 +0100

    Initial Commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Silva Kirsimae on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
