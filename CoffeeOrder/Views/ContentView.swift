10a7ccb99c48eaff58a3e407a0ab7f3cbf3fbc0d
Silva Kirsimae <silva.kirsimae-cic.netherlands@ibm.com>
2023-01-20 16:36:50 +0100

    Update ContentView with toolbar items and handle actions

--- CoffeeOrder/Views/ContentView.swift
+++ CoffeeOrder/Views/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject private var orderListVM = OrderListViewModel()
     
+    @State private var showModal: Bool = false
+    
     var body: some View {
         NavigationView {
             OrderListView(orders: self.orderListVM.orders)
                 .navigationTitle("Coffee Orders")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: reloadOrders) {
+                            Image(systemName: "arrow.clockwise")
+                                .foregroundColor(.white)
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: showAddCoffeeOrderView) {
+                            Image(systemName: "plus")
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                .sheet(isPresented: self.$showModal) {
+                    AddCoffeeOrderView(isPresented: self.$showModal)
+                }
         }
     }
+    
+    private func reloadOrders() {
+        self.orderListVM.fetchOrders()
+    }
+    
+    private func showAddCoffeeOrderView() {
+        self.showModal = true
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
