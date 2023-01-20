490a6f0290de5f2d575303ca0f89b4f66fc56577
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-20 11:10:49 +0100

    Create createCoffeeOrder request

---
+++ CoffeeOrder/Models/CreateOrderResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeOrder
+//
+//  Created by Silva Kirsimae on 20/01/2023.
+//
+
+import Foundation
+
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
