ebc6772b9f490f0b9113358b10aabb558cc4d24c
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-20 10:56:11 +0100

    Create entry view for the app

--- CoffeeOrder/Views/ContentView.swift
+++ CoffeeOrder/Views/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var orderListVM = OrderListViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            OrderListView(orders: self.orderListVM.orders)
+                .navigationTitle("Coffee Orders")
         }
-        .padding()
     }
 }
 
