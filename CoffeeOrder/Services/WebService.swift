a9ed8b1642e97ed0e1905667833e8b123bc34351
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-20 10:55:35 +0100

    Set up web service to get all orders

---
+++ CoffeeOrder/Services/WebService.swift
@@ -0,0 +1,32 @@
+//
+//  WebService.swift
+//  CoffeeOrder
+//
+//  Created by Silva Kirsimae on 19/01/2023.
+//
+
+import Foundation
+
+class WebService {
+    func getAllOrders(completion: @escaping ([Order]?) -> Void) {
+        guard let url = URL(string: "https://island-bramble.glitch.me/orders") else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            let orders = try? JSONDecoder().decode([Order].self, from: data)
+            DispatchQueue.main.async {
+                completion(orders)
+            }
+        }
+        .resume()
+    }
+}
