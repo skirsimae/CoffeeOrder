f0bdea654d5a090a43669986f650199e77e33c27
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-20 12:00:03 +0100

    Create Coffee Order view

---
+++ CoffeeOrder/Views/AddCoffeeOrderView.swift
@@ -0,0 +1,61 @@
+//
+//  AddCoffeeOrderView.swift
+//  CoffeeOrder
+//
+//  Created by Silva Kirsimae on 20/01/2023.
+//
+
+import SwiftUI
+
+struct AddCoffeeOrderView: View {
+    
+    @ObservedObject private var addCoffeeOrderVM = AddCoffeeOrderViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("INFORMATION").font(.body)) {
+                        TextField("Enter name", text: self.$addCoffeeOrderVM.name)
+                    }
+                    
+                    Section(header: Text("SELECT COFFEE").font(.body)) {
+                        ForEach(addCoffeeOrderVM.coffeeList, id: \.name) {
+                            coffee in
+                            
+                            CoffeeCellView(coffee: coffee, selection: self.$addCoffeeOrderVM.coffeeName)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddCoffeeOrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCoffeeOrderView()
+    }
+}
+
+struct CoffeeCellView: View {
+    
+    let coffee: CoffeeViewModel
+    @Binding var selection: String
+    
+    var body: some View {
+        HStack {
+            Image(coffee.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+            Text(coffee.name)
+                .font(.title)
+                .padding([.leading],20)
+            Spacer()
+            Image(systemName: self.selection == self.coffee.name ? "checkmark": "").padding()
+        }.onTapGesture {
+            self.selection = self.coffee.name
+        }
+    }
+}
