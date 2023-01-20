a3fbab9a671cbf01139db5e2a82f3c9788fe2781
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-20 11:59:18 +0100

    Create CoffeeViewModel

---
+++ CoffeeOrder/View Models/CoffeeViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeViewModel.swift
+//  CoffeeOrder
+//
+//  Created by Silva Kirsimae on 20/01/2023.
+//
+
+import Foundation
+
+class CoffeeListViewModel {
+    var coffeeList: [CoffeeViewModel] = [CoffeeViewModel]()
+}
+
+struct CoffeeViewModel {
+    var coffee: Coffee
+    
+    init(coffee: Coffee) {
+        self.coffee = coffee
+    }
+    
+    var name: String {
+        return self.coffee.name
+    }
+    
+    var imageURL: String {
+        return self.coffee.imageURL
+    }
+    
+    var price: Double {
+        return self.coffee.price
+    }
+}
