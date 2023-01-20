63378f1a4f21df20e58715b48da63738e6f6631d
Silva Kirsimae <silva.kirsimae@jumbo.com>
2023-01-20 10:55:03 +0100

    Add Order model

---
+++ CoffeeOrder/Models/Order.swift
@@ -0,0 +1,16 @@
+//
+//  Order.swift
+//  CoffeeOrder
+//
+//  Created by Silva Kirsimae on 19/01/2023.
+//
+
+import Foundation
+
+struct Order: Codable {
+    let name: String
+    let size: String
+    let coffeeName: String
+    let total: Double
+    
+}
